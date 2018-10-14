a6178f6a30284b3d224f2e300d8e34aa2d9e9597
Stolof <olof.bergm@gmail.com>
2018-10-14 11:54:00 +0200

    main available for input done

--- devXBackend/src/mean_pixel_value.py
+++ devXBackend/src/mean_pixel_value.py
@@ -7,10 +7,7 @@ from PIL import Image
 
 #### TODO
 # Outprint a mean pixel value picture
-# Fix main function when joining sliced image
-# Divide input_picture to 4 or 8 smaler picture. Iterative over the joining
 # Add error checks for easier development.
-
 
 # Input is gonna be an img, check if the img size is 32, 32, 3, return bgr right now.
 def mean_pixel_value(img):
@@ -58,19 +55,11 @@ def make_album_list():
     return album_list
 
 # Function with parameter
-def make_album_list3(genre):
+def make_album_list_input(genre):
     album_list = []
     files = os.listdir(genre)
     for fil in files:
         album_list.append(cv2.imread(genre + '/' + fil))
-    return album_list
-
-# List for 32 pixels
-def make_album_list2():
-    album_list = []
-    files = os.listdir('pictures') # featured 32 pixels?
-    for fil in files:
-        album_list.append(cv2.imread('pictures/' + fil))
     return album_list
 
 # Resize input file cv2 function, with a write, Maybe better with more pixels
@@ -87,7 +76,6 @@ def resize_album_img(img, w=32, h=32):
 def input_to_fragments(img):
     os.chdir('fragments_of_inputs')
     info = image_slicer.slice(img, 256)
-
     return info
 
 # Function
@@ -103,6 +91,7 @@ def fragments_to_output(chunks):
 # Main function, opacity
 def main(): # Genre should be string equal to the directory names this is the input in main
     #test_pic = cv2.imread('input_pic.jpg')
+    #input_dict = np.load(genre + '_dict.npy').item()
     chill_dict = np.load('chill_dict.npy').item()
     resized_img = resize_input_img(test_pic2) # test_pic2 for Obama
     album_list = make_album_list() # 3 for input
@@ -114,50 +103,46 @@ def main(): # Genre should be string equal to the directory names this is the in
             color_pic = np.resize(temp, (1,1,3))
             color = np.round(np.array(color_pic))
             color = tuple(color[0][0])
-            if color in chill_dict.keys():
-                closest_image_filenames = chill_dict[color]
+            if color in chill_dict.keys(): #input_dict
+                closest_image_filenames = chill_dict[color] # input_dict
                 rand = random.randint(0,len(closest_image_filenames) - 1)
                 closest_image_filename = closest_image_filenames[rand]
             else:
-               new_color = closest_dict(list(chill_dict.keys()), color)
-               closest_image_filenames = chill_dict[new_color]
+               new_color = closest_dict(list(chill_dict.keys()), color) #input_dict
+               closest_image_filenames = chill_dict[new_color] #input_dict
                rand = random.randint(0,len(closest_image_filenames) - 1)
                closest_image_filename = closest_image_filenames[rand]
             output_chunk_list[x][y] = closest_image_filename
     output_pic = fragments_to_output(output_chunk_list)
     cv2.imwrite('result.jpg', output_pic)
 
-# Olofs modified old
-def main3():
-   resized_img = resize_input_img(test_pic2)
-   album_list = make_album_list()
-   output_chunk_list = [['' for _ in range(0,32)] for _ in range(0,32)]
-   for x in range(0,32):
-       for y in range(0,32):
-           temp = resized_img[(0 + x*16):(16 + x*16),(0 + y*16):(16 + y*16)]
-           #pic = cv2.imread(tile.filename)
-           closest_image = closest_img(temp, album_list)
-           output_chunk_list[x][y] = closest_image
-           #print(closest_image)
-   output_pic = fragments_to_output(output_chunk_list)
-   cv2.imwrite('result_main3.jpg', output_pic)
-
-# Olofs old with slice
-def main2():
-   resize_input_img(test_pic2)
-   album_list = make_album_list()
-   print(len(album_list)) # Number of albums available
-   tiles = image_slicer.slice('resize_input_image.jpg', 256) # wrong dir
-   for tile in tiles:
-       if tile.filename == 'resize_input_image_16_02.png': # Stop the opening
-           break
-       print(tile.filename)
-       pic = cv2.imread(tile.filename)
-       closest_image = closest_img(pic, album_list)
-       cv2.imwrite(tile.filename, closest_image)
-       tile.image = Image.open(tile.filename) # Probably cant open more than ca 200 tiles
-   final_pic = image_slicer.join(tiles)
-   final_pic.save('result_slice.jpg')
+# Main function, with input
+def main_input(genre): # genre is a string, should match the directory name
+    test_pic = cv2.imread('input_pic.jpg')
+    input_dict = np.load(genre + '_dict.npy').item()
+    resized_img = resize_input_img(test_pic)
+    album_list = make_album_list_input(genre)
+    print(len(album_list)) # Number of albums availale
+    output_chunk_list = [['' for _ in range(0,32)] for _ in range(0,32)]
+    for x in range(0,32):
+        for y in range(0,32):
+            temp = resized_img[(0 + x*16):(16 + x*16),(0 + y*16):(16 + y*16)]
+            color_pic = np.resize(temp, (1,1,3))
+            color = np.round(np.array(color_pic))
+            color = tuple(color[0][0])
+            if color in input_dict.keys(): #input_dict
+                closest_image_filenames = input_dict[color] # input_dict
+                rand = random.randint(0,len(closest_image_filenames) - 1)
+                closest_image_filename = closest_image_filenames[rand]
+            else:
+               new_color = closest_dict(list(input_dict.keys()), color) #input_dict
+               closest_image_filenames = input_dict[new_color] #input_dict
+               rand = random.randint(0,len(closest_image_filenames) - 1)
+               closest_image_filename = closest_image_filenames[rand]
+            output_chunk_list[x][y] = closest_image_filename
+    output_pic = fragments_to_output(output_chunk_list)
+    cv2.imwrite('result.jpg', output_pic)
+    return output_pic # Returns output pic.
 
 ### Values for testing functions
 #test_pic = cv2.imread('pictures/winrar.png')
